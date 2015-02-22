d9845072e3a6e6556788f4c183e35614ca7462cf
Hans Melby <hanshell@stud.ntnu.no>
2015-02-22 17:04:53 +0100

    Changed to correct variables

--- voiceclassification.m
+++ voiceclassification.m
@@ -3,9 +3,8 @@ function [ classification ] = voiceclassification( audio_segment )
 % Analysis a sound signal segment and determines whether it is voiced or
 % unvoiced based on a set threshold value
 %   
-myRecording_sample=myRecording(1:320);
-xcorr_myRecording_sample=xcorr(myRecording_sample);
-xcorr_medianfiltered=medfilt1(xcorr_myRecording_sample, 10);
+xcorr_audio_segment=xcorr(audio_segment);
+xcorr_medianfiltered=medfilt1(xcorr_audio_segment, 10);
 threshold=0.2;
 
 detection=-1; % 0 if voiced, 1 if unvoiced;

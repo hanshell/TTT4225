0183308fdc5249f8d330ad683d6c2a4e45a4615c
Jon Magnus <jonmmh8@gmail.com>
2015-02-22 19:12:46 +0100

    Lol

--- Speech Analysis/addPitch.m
+++ Speech Analysis/addPitch.m
@@ -1,16 +1,22 @@
-function [ pitches ] = addPitch( wasVoiced, prevPitchPos, pitchPeriod, start, stop )
+function [ temp_pitches , prevPp ] = addPitch( wasVoiced, prevPitchPos, pitchPeriod, start, stop )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    nxtIndex = start;
-    while nxtIndex <= stop;
+    %nxtIndex = start;
+    prevPp = 0;
+    temp_pitches = zeros(1,(stop-start)+1);
+    length(temp_pitches)
+
         if wasVoiced;
-            pitch(prevPitchPos+pitchPeriod) = 1;
+            temp_pitches((start - prevPitchPos + pitchPeriod)  : pitchPeriod : stop) = 1;
+         
         else
-            pitch(nxtIndex) = 1;
+
+            temp_pitches(1 : pitchPeriod : end) = 1
+            
         end
-        nxtIndex = nxtIndex + pitchPeriod;
-        prevPitchPos = prevPitchPos+pitchPeriod;
-    end
+    %    nxtIndex = nxtIndex + pitchPeriod;
+     %   prevPitchPos = prevPitchPos+pitchPeriod;
+        
 
 end
 

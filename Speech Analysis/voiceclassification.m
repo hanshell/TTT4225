b9afbc4acc8e9ac27af1255137e0d48d54b2cc40
Jon Magnus <jonmmh8@gmail.com>
2015-02-22 17:07:03 +0100

    add to folder

---
+++ Speech Analysis/voiceclassification.m
@@ -0,0 +1,26 @@
+function [ classification ] = voiceclassification( audio_segment )
+
+% Analysis a sound signal segment and determines whether it is voiced or
+% unvoiced based on a set threshold value
+%   
+xcorr_audio_segment=xcorr(audio_segment);
+xcorr_medianfiltered=medfilt1(xcorr_audio_segment, 10);
+threshold=0.2;
+
+detection=-1; % 0 if voiced, 1 if unvoiced;
+
+peaks=findpeaks(xcorr_medianfiltered);
+
+ratio=mean(peaks)/max(peaks);
+
+if ratio>=threshold
+    detection=1;
+else
+    detection=0;
+end
+
+classification=detection;
+
+
+end
+

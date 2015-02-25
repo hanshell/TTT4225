2123f0929a3372996b900d24f381c5f06bcfcb70
Jon Magnus <jonmmh8@gmail.com>
2015-02-25 20:22:26 +0100

    deda

--- Speech Analysis/main3.m
+++ Speech Analysis/main3.m
@@ -5,9 +5,8 @@ filename = 'files/original.wav'; % name/location of audiofile to be read
 n_frame = 480; % lenght of frame we multiply with our hamming window
 n_step = 320; % how far we each step is
 
-residual = getResidual(s, n_frame, n_step);
-
-
+% the residual signal is calculated in our function and returned to the array.
+residual = getResidual(s, n_frame, n_step); 
 
 
 
--- Speech Analysis/task1_2.m
+++ Speech Analysis/task1_2.m
@@ -1,4 +1,4 @@
-filename = 'files/original.wav';
+filename = 'files/baba1.wav';
 
 % 16000 * (20*10^-3)
 N_frame = 320;

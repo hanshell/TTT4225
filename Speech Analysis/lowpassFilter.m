fb1307a928c22d6687b61a1df5c34a66a945460f
Jon Magnus <jonmmh8@gmail.com>
2015-02-25 20:37:29 +0100

    dss

--- Speech Analysis/lowpassFilter.m
+++ Speech Analysis/lowpassFilter.m
@@ -2,11 +2,10 @@ function [ filtered_signal ] = lowpassFilter( signal )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
     filename = 'files/original.wav'; % name/location of audiofile to be read
-    [s, Fs] = audioread(filename);
     
     order = 14;
-    f = [0 0.1 0.2 0.25 0.35 0.45 0.7 1];
-    a = [1 1 1 1 0 0 0 0];
+    f = [0 0.1 0.2 0.25 0.35 0.45 0.55 0.65 0.7 1];
+    a = [1 1 1 1 0 0 0 0 0 0];
     
     A = firpm(order, f, a);
     [H,w] = freqz(A);
--- Speech Analysis/main3.m
+++ Speech Analysis/main3.m
@@ -7,6 +7,12 @@ n_step = 320; % how far we each step is
 
 % the residual signal is calculated in our function and returned to the array.
 residual = getResidual(s, n_frame, n_step); 
-
-
+lp_residual = lowpassFilter(residual);
+[H,w] = freqz(residual);
+[H2,w2] = freqz(lp_residual);
+figure
+subplot(2,1,1)
+plot(w/pi,abs(H))
+subplot(2,1,2)
+plot(w2/pi,abs(H2))
 

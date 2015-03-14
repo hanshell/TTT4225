743a3e862fff113a9bbce542ef8bd2ce0778b4dd
Hans Melby <hanshell@stud.ntnu.no>
2015-03-14 16:52:45 +0100

    gnraei

--- cProgram/interpolation.c
+++ cProgram/interpolation.c
@@ -9,7 +9,7 @@ float *upsample_frame(float frame[], int frame_size, int upsampling_factor){
     float *upsampled_frame=calloc(frame_size*upsampling_factor, sizeof(float));
     
     int i;
-    for(i=0; i<frame_size*upsampling_factor; i++){
+    for(i=0; i<frame_size; i++){
         upsampled_frame[i*upsampling_factor]=frame[i];
     }
     return upsampled_frame;
